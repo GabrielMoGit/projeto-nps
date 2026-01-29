7745abcee1c1a35d517df86fc34fa69f836b5c72
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-01-29 01:00:58 -0300

    Criada as rotas teste de GET e POST iniciais

--- src/server.ts
+++ src/server.ts
@@ -11,4 +11,14 @@ DELETE - Deletar
 PATCH - Alteração específica
 */
 
+// http://localhost:3333/users
+app.get("/", (request, response) => {
+    return response.json({message: "Hello world"});
+});
+
+// http://localhost:3333/ 
+app.post("/", (request, response) => {
+    return response.json({message: "Os dados foram salvos com sucesso"});   
+});
+
 app.listen(3333, ()=> console.log("Servidor rodando!"));
