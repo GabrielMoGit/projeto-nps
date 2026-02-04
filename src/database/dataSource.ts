6c07d234fcb0736adcbf391b00844fcd2332f38c
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-04 20:04:59 -0300

    Adicionado o caminho de entidade para a pasta model

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "src/database/database.sqlite",
-  entities: ["src/entities/*.ts"],
+  entities: ["src/models/*.ts"],
   migrations: ["src/database/migrations/*.ts"],
   synchronize: false,
 });
