6df2c0459934a66f118223bc4d472fb4d1df8bdc
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-04 16:43:31 -0300

    Adicionado o caminho da migration no arquivo de orm e o arquivo da migration de usuarios

---
+++ src/database/migrations/1770181854362-CreateUsers.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsers1770181854362 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
