9ad8d647018c4094471bbbc78e26da3e652d8c6c
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-04 23:37:05 -0300

    Adicionado apenas alguns comentarios para ficar mais fácil de estudar pelo código posteriormente

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,7 @@ import {v4 as uuid} from 'UUID'
 class User{ //ATRIBUTOS DA TABELA
 
     @PrimaryColumn()
-    readonly id: string;
+    readonly id: string; //READONLY FAZ COM QUE NÃO POSS SER ALTERADO
     
     @Column()
     name: string;
@@ -15,12 +15,12 @@ class User{ //ATRIBUTOS DA TABELA
     @Column()
     email: string;
     
-    @CreateDateColumn()
+    @CreateDateColumn() //CRIA AUTOMATICAMENTE A DATA, NO MOMENTO DA CRIAÇÃO DO REGISTRO
     created_at: Date;
 
     constructor(){
-        if(!this.id){
-            this.id = uuid()
+        if(!this.id){ //VERIFICA SE O USUARIO NÃO POSSÚI UM ID
+            this.id = uuid() //CASO NÃO POSSUA ELE GERA UM ID PARA O USUÁRIO
         }
     }
 }
