04a98731fc4d87ec07e410e163f3ac4959671769
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-19 17:35:22 -0300

    ALterado para usar os métodos do repositorio próprio do sistema

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,16 +1,21 @@
 import { Request, Response } from "express";
 import { AppDataSource } from "../database/dataSource"; //INSTANCIA DE CONEXAO COM O BANCO DE DADOS
 import { User } from "../models/User"; //ENTIDADE USER QUE REPRESENTA A TABELA USER DO BANCO
+import { UsersRepository } from "../repositories/UsersRepository";
+
+interface UserInformation{
+    name: string;
+    email: string;
+}
 
 class UsersController{ //DEFINIÇÃO DA CLASSE
     async create(request: Request, response: Response){ //MÉTODO QUE SERÁ CHAMADO QUANDO FIZER REQUISIÇÃO DE CRIAR USUARIO
         const {name, email} = request.body; //DADOS ENVIADOS NO CORPO DA REQUISIÇÃO
 
-        const userRepository = AppDataSource.getRepository(User); //REPORITORIO DO TYPEORM PARA EXECUTAR OPERAÇÃO NA TABELA USER
+        const userRepository = new UsersRepository(); //REPORITORIO DO TYPEORM PARA EXECUTAR OPERAÇÃO NA TABELA USER
 
-        const userAlreadyExists = await userRepository.findOne({ //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
-            where: {email}, 
-        });
+        const userAlreadyExists = await userRepository.findByEmail(email); //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+
 
         if(userAlreadyExists){ //SE O USUARIO JÁ EXISTIR...
             return response.status(400).json({
@@ -18,11 +23,7 @@ class UsersController{ //DEFINIÇÃO DA CLASSE
             });
         }
         
-        const user = userRepository.create({ //CRIA O OBJETO USER COM OS DADOS RECEBIDOS
-            name, email         
-        })
-
-        await userRepository.save(user); //SALVA O USUARIO NO BANCO DE DADOS
+        const user = userRepository.createAndSave(name, email); //CRIA E SALVA O OBJETO USER COM OS DADOS RECEBIDOS
 
         return response.json(user);
     }
