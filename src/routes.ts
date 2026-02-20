74dbaf21d33a5006776676560c729dd35e206385
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-20 02:03:14 -0300

    Adicionato a rota para surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import { UsersController } from "./controllers/usersController"
+import { SurveysRepository } from './repositories/SurveysRepository';
+import { SurveysController } from './controllers/surveysController';
 
 const router = Router();
 
 const usersController = new UsersController(); 
+const surveysController = new SurveysController();
 
 router.post("/users", usersController.create);
+router.post("/surveys", surveysController.create);
 
 export {router};
