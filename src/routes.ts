87df6f606e79214a478e520f76945204f1af9fa7
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-20 02:24:39 -0300

    Criada a rota get para listar as pesquisas

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,10 @@ const router = Router();
 const usersController = new UsersController(); 
 const surveysController = new SurveysController();
 
+//ROTAS PARA USERS
 router.post("/users", usersController.create);
-router.post("/surveys", surveysController.create);
 
+//ROTAS PARA SURVEYS
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 export { router };
