4da72ea970f9157eab872cc94d67f80ac0f70c64
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-20 05:05:00 -0300

    criado o primeiro teste para checar a ferramenta jest, alterado o tsconfig para poder aceitar o jest, funcionando como esperado

---
+++ src/__tests__/First.test.ts
@@ -0,0 +1,9 @@
+
+
+describe("First", () => {
+    it("deve ser possível somar 2 números", () => {   //O QUE O TESTE VAI FAZER
+        expect(2 + 2).toBe(4); //O QUE A GENTE ESPERA DO TESTE
+    });
+});
+
+export{};
