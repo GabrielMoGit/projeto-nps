13209c5fd651417028d7590321bf155971b4682b
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-20 02:02:12 -0300

    Configurado o controller de surveys para receber a requisição e retornar a criação da pesquisa

--- src/controllers/surveysController.ts
+++ src/controllers/surveysController.ts
@@ -1,9 +1,17 @@
-import {request, response} from "express";
+import {Request, Response} from "express";
+import { SurveysRepository } from "../repositories/SurveysRepository";
 
 class SurveysController{
 
-    async create(request: Request, responde: Response){
-        
+    async create(request: Request, response: Response){
+        const {title, description} = request.body;  
+
+        const surveysRepository = new SurveysRepository();
+
+        const survey = await surveysRepository.createAndSave(title, description);
+
+        return response.status(201).json(survey);
+
     }
 }
 
