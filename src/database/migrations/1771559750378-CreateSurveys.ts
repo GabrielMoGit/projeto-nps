9ea203ae73b9860bab8475fc6bdb21dc84b721b7
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-20 00:58:33 -0300

    Criado migration surveys

---
+++ src/database/migrations/1771559750378-CreateSurveys.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateSurveys1771559750378 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
