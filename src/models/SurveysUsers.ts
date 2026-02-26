fca496e3ea3318277818ccdc794af3a128042550
GabrielMoGit <Gabriel.moreira97@outlook.pt>
2026-02-25 21:58:19 -0300

    Configurado o arquivo de model para SurveysUsers

--- src/models/SurveysUsers.ts
+++ src/models/SurveysUsers.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, ForeignKey, PrimaryColumn } from "typeorm";
-import { v4 as uuid} from "uuid";
-/*
-@Entity("Surveys_Users")
+import { v4 } from "uuid";
+
+@Entity("SurveysUsers")
 
 class SurveysUsers{
     
@@ -9,9 +9,23 @@ class SurveysUsers{
     readonly id: string;
 
     @Column()
-    
+    user_id: string;
+
+    @Column()
+    survey_id: string;
+
+    @Column()
+    value: number;
+
+    @CreateDateColumn() //CRIA AUTOMATICAMENTE A DATA, NO MOMENTO DA CRIAÇÃO DO REGISTRO
+    created_at: Date;
+
+    constructor(){
+        if(!this.id){ //VERIFICA SE O USUARIO NÃO POSSÚI UM ID
+            this.id = v4() //CASO NÃO POSSUA ELE GERA UM ID PARA O USUÁRIO
+        }
+    }
 
 }
 
 export {SurveysUsers};
-*/
